61a284473a3af5f18e35ace38e3d4e68349ac676
jair <jair.r04@gmail.com>
2019-01-08 20:48:07 -0600

    SDH Hybris Commerce - Gasolina YForm - Princilap Menu

--- hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/pages/forms/Example.java
+++ hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/pages/forms/Example.java
@@ -1,7 +1,7 @@
 /**
  *
  */
-package de.hybris.sdh.storefront.controllers.pages.yforms;
+package de.hybris.sdh.storefront.controllers.pages.forms;
 
 import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
 import de.hybris.platform.acceleratorstorefrontcommons.breadcrumb.ResourceBreadcrumbBuilder;
@@ -11,43 +11,47 @@ import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
 
 import javax.annotation.Resource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-/**
- * @author yjwhoinu
- *
- */
+
 
 @Controller
-@RequestMapping("/gasolina")
-public class Gasolina extends AbstractSearchPageController
+@RequestMapping("/example")
+public class Example extends AbstractSearchPageController
 {
-	private static final Logger LOG = LoggerFactory.getLogger(Gasolina.class);
 	private static final String BREADCRUMBS_ATTR = "breadcrumbs";
 	private static final String TEXT_ACCOUNT_PROFILE = "text.account.profile";
 
 	// CMS Pages
-	private static final String GASOLINA_LIQUIDADOR_CMS_PAGE = "cms-page-gasolina-liquidador";
+	private static final String YFORM_CMS_PAGE = "update-example-form";
+
 
 	@Resource(name = "accountBreadcrumbBuilder")
 	private ResourceBreadcrumbBuilder accountBreadcrumbBuilder;
 
-	@RequestMapping(value = "/gasolina-liquidador-form", method = RequestMethod.GET)
+	@RequestMapping(value = "/update-example-form", method = RequestMethod.GET)
 	@RequireHardLogIn
 	public String updateExampleForm(final Model model) throws CMSItemNotFoundException
 	{
-		LOG.info("---------------- CMS Page Gasolina Liquidador - GET Method --------------------------");
+		//System.out.println("---------------- Hola entro a updateExampleForm --------------------------");
 
-		storeCmsPageInModel(model, getContentPageForLabelOrId(GASOLINA_LIQUIDADOR_CMS_PAGE));
-		setUpMetaDataForContentPage(model, getContentPageForLabelOrId(GASOLINA_LIQUIDADOR_CMS_PAGE));
+		//model.addAttribute("dataPage", "hola como estas esta es una prueba");
+
+		storeCmsPageInModel(model, getContentPageForLabelOrId(YFORM_CMS_PAGE));
+		setUpMetaDataForContentPage(model, getContentPageForLabelOrId(YFORM_CMS_PAGE));
 		model.addAttribute(BREADCRUMBS_ATTR, accountBreadcrumbBuilder.getBreadcrumbs(TEXT_ACCOUNT_PROFILE));
 		model.addAttribute(ThirdPartyConstants.SeoRobots.META_ROBOTS, ThirdPartyConstants.SeoRobots.NOINDEX_NOFOLLOW);
 
 		return getViewForPage(model);
+	}
+
+	@RequestMapping(value = "/update-example-form", method = RequestMethod.POST)
+	@RequireHardLogIn
+	public void postUpdateExampleForm()
+	{
+		System.out.print("hola como estas");
 	}
 
 }
