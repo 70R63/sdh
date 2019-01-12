ac070b5478625af704ef3fc6b8676321cbb74da8
jcarrillo <jesus.carrillo1987@gmail.com>
2019-01-12 03:18:57 -0600

    Cambios de texto, el login, etc.

---
+++ hybris/hybris/bin/custom/sdh/sdhcore/src/de/hybris/sdh/core/exceptions/NotAValidEmailException.java
@@ -0,0 +1,43 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.sdh.core.exceptions;
+
+
+import org.springframework.security.core.AuthenticationException;
+
+
+/**
+ * UnkownResource validation exception used to throw validation errors. <br/>
+ */
+
+public class NotAValidEmailException extends AuthenticationException
+{
+
+	/**
+	 * @param msg
+	 */
+	public NotAValidEmailException(final String msg)
+	{
+		super(msg);
+		// XXX Auto-generated constructor stub
+	}
+
+	/**
+	 * @param msg
+	 * @param t
+	 */
+	public NotAValidEmailException(final String msg, final Throwable t)
+	{
+		super(msg, t);
+		// XXX Auto-generated constructor stub
+	}
+
+}
