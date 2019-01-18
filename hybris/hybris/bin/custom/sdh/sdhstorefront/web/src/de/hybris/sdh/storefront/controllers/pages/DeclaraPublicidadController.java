708f6ab653670e1d40d63944d77d14d053b9984d
MariaTorres <tosl92@gmail.com>
2019-01-18 16:01:36 -0600

    no message

--- hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/pages/DeclaraPublicidadController.java
+++ hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/pages/DeclaraPublicidadController.java
@@ -6,18 +6,8 @@ package de.hybris.sdh.storefront.controllers.pages;
 import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractPageController;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
-<<<<<<< HEAD
-import de.hybris.platform.servicelayer.session.SessionService;
-import de.hybris.platform.servicelayer.user.UserService;
 import de.hybris.sdh.storefront.forms.DeclaPublicidadController;
 
-import javax.annotation.Resource;
-
-import org.apache.log4j.Logger;
-=======
-import de.hybris.sdh.storefront.forms.DeclaPublicidadController;
-
->>>>>>> dev
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -34,10 +24,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/contribuyentes/publicidadexterior/declaracion")
 public class DeclaraPublicidadController extends AbstractPageController
 {
-<<<<<<< HEAD
-	private static final Logger LOG = Logger.getLogger(DeclaraPublicidadController.class);
-=======
->>>>>>> dev
+
 	private static final String ERROR_CMS_PAGE = "notFound";
 	private static final String TEXT_ACCOUNT_PROFILE = "text.account.profile";
 
@@ -46,24 +33,11 @@ public class DeclaraPublicidadController extends AbstractPageController
 	private static final String REDIRECT_TO_DECLARACIONES_PUBLICIDAD_PAGE = REDIRECT_PREFIX
 			+ "/contribuyentes/publicidadexterior/declaracion";
 
-<<<<<<< HEAD
-	@Resource(name = "sessionService")
-	SessionService sessionService;
-
-	@Resource(name = "userService")
-	UserService userService;
-
-
-=======
->>>>>>> dev
 	@RequestMapping(value = "/contribuyentes/publicidadexterior/declaracion", method = RequestMethod.GET)
 	@RequireHardLogIn
 	public String declaraPublicidadpage(final Model model, final RedirectAttributes redirectModel) throws CMSItemNotFoundException
 	{
-<<<<<<< HEAD
 
-=======
->>>>>>> dev
 		final DeclaPublicidadController dataForm = new DeclaPublicidadController();
 		model.addAttribute("dataForm", dataForm);
 		storeCmsPageInModel(model, getContentPageForLabelOrId(DECLARACION_PUBLICIDAD_CMS_PAGE));
