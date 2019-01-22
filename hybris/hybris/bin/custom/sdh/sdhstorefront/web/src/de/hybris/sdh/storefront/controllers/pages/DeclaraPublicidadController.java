7d7fdcc4e8e04bef97224fad8d3cfcd17f1bacb3
MariaTorres <tosl92@gmail.com>
2019-01-22 16:01:33 -0600

    Consumo de servicio de Calculo Publicidad

--- hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/pages/DeclaraPublicidadController.java
+++ hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/pages/DeclaraPublicidadController.java
@@ -6,8 +6,20 @@ package de.hybris.sdh.storefront.controllers.pages;
 import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractPageController;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
+import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.servicelayer.session.SessionService;
+import de.hybris.platform.servicelayer.user.UserService;
+import de.hybris.sdh.core.pojos.requests.CalcPublicidadRequest;
+import de.hybris.sdh.core.pojos.requests.ConsultaContribuyenteBPRequest;
+import de.hybris.sdh.core.pojos.responses.CalcPublicidadResponse;
+import de.hybris.sdh.core.services.SDHCalPublicidadService;
+import de.hybris.sdh.core.services.SDHConsultaContribuyenteBPService;
 import de.hybris.sdh.storefront.forms.DeclaPublicidadController;
 
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,6 +36,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/contribuyentes/publicidadexterior/declaracion")
 public class DeclaraPublicidadController extends AbstractPageController
 {
+	private static final Logger LOG = Logger.getLogger(DeclaraPublicidadController.class);
 
 	private static final String ERROR_CMS_PAGE = "notFound";
 	private static final String TEXT_ACCOUNT_PROFILE = "text.account.profile";
@@ -33,11 +46,69 @@ public class DeclaraPublicidadController extends AbstractPageController
 	private static final String REDIRECT_TO_DECLARACIONES_PUBLICIDAD_PAGE = REDIRECT_PREFIX
 			+ "/contribuyentes/publicidadexterior/declaracion";
 
-	@RequestMapping(value = "/contribuyentes/publicidadexterior/declaracion", method = RequestMethod.GET)
+
+	@Resource(name = "sessionService")
+	SessionService sessionService;
+
+	@Resource(name = "userService")
+	UserService userService;
+
+	@Resource(name = "sdhCalPublicidadService")
+	SDHCalPublicidadService sdhCalPublicidadService;
+
+	@Resource(name = "sdhConsultaContribuyenteBPService")
+	SDHConsultaContribuyenteBPService sdhConsultaContribuyenteBPService;
+
+	@RequestMapping(method = RequestMethod.GET)
 	@RequireHardLogIn
 	public String declaraPublicidadpage(final Model model, final RedirectAttributes redirectModel) throws CMSItemNotFoundException
 	{
+		final CustomerModel customerModel = (CustomerModel) userService.getCurrentUser();
 
+		final ConsultaContribuyenteBPRequest consultaContribuyenteBPRequest = new ConsultaContribuyenteBPRequest();
+
+		consultaContribuyenteBPRequest.setNumBP(customerModel.getNumBP());
+
+		final CalcPublicidadRequest calcPublicidadRequest = new CalcPublicidadRequest();
+
+		/*
+		 * calcPublicidadRequest.setNumBP(); calcPublicidadRequest.setNumResolu(); calcPublicidadRequest.setNumForm();
+		 * calcPublicidadRequest.setAnoGravable(); calcPublicidadRequest.setOpcionUso();
+		 * calcPublicidadRequest.setFechNotif(); calcPublicidadRequest.setLugarInstala();
+		 * calcPublicidadRequest.setDireccion(); calcPublicidadRequest.setPlaca();
+		 * calcPublicidadRequest.setOrientacionValla(); calcPublicidadRequest.setTamanoValla();
+		 * calcPublicidadRequest.setTipoIDcontrib(); calcPublicidadRequest.setIDcontrib();
+		 * calcPublicidadRequest.setMunicipioContrib(); calcPublicidadRequest.setTipoIDdeclara();
+		 * calcPublicidadRequest.setIDdeclarante();
+		 */
+
+
+
+		try
+		{
+			final ObjectMapper mapper = new ObjectMapper();
+			mapper.configure(org.codehaus.jackson.map.DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+			final CalcPublicidadResponse calcPublicidadResponse = mapper
+					.readValue(sdhCalPublicidadService.calcPublicidad(calcPublicidadRequest), CalcPublicidadResponse.class);
+
+			final DeclaPublicidadController declaPublicidad = new DeclaPublicidadController();
+
+			//declaPublicidad.setNumResol(customerModel.getNumBP);
+			//declaPublicidad.setNumResol(sdhCalPublicidadService.getNumResol);
+
+
+		}
+
+
+		catch (final Exception e)
+		{
+			// XXX Auto-generated catch block
+			//LOG.error("error getting customer info from SAP for rit page: " + e.getMessage());
+			//GlobalMessages.addErrorMessage(model, "mirit.error.getInfo");
+		}
+		System.out.println("prueba dos millones");
+		//declaPublicidad.setNumResol(customerModel.getNumBP);
 		final DeclaPublicidadController dataForm = new DeclaPublicidadController();
 		model.addAttribute("dataForm", dataForm);
 		storeCmsPageInModel(model, getContentPageForLabelOrId(DECLARACION_PUBLICIDAD_CMS_PAGE));
