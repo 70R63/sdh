af89805d9ba0554ad83a68956d17904e7a13db63
MariaTorres <tosl92@gmail.com>
2019-04-23 20:19:32 -0500

    No se muestra registro vacío en la tala de CDU´s y Radicados de Delineación Urbana

--- hybris/hybris/bin/custom/sdh/sdhstorefront/web/webroot/_ui/responsive/common/js/acc.duBotonVer.js
+++ hybris/hybris/bin/custom/sdh/sdhstorefront/web/webroot/_ui/responsive/common/js/acc.duBotonVer.js
@@ -1,6 +1,6 @@
 ACC.duBotonVer = {
 
-	_autoload : [ "bindLabelVerButton" ],
+	_autoload : [ "bindLabelVerButton","bindCheckBoxSelect","bindGeneraDeclaracionButton","bindDialogGasolina" ],
 
 	
 	bindLabelVerButton : function() {
@@ -8,18 +8,18 @@ ACC.duBotonVer = {
 			e.preventDefault();
 			
 
- 	       var tipoValla = $.trim($(this).attr("data-cdu")); 	       
+ 	       var valorCDU = $.trim($(this).attr("data-cdu")); 	       
 
- 	       if (tipoValla == "") {
+ 	       if (valorCDU == "") {
 				alert("Por favor, selecciona un CDU");
 				return;
 			}
 
-			$("#selectedTipoValla").val(tipoValla);
+			$("#selectedCDU").val(valorCDU);
 
 			var data = {};
 
-			data.cdu = tipoValla;
+			data.cdu = valorCDU;
 
 			$.ajax({
 				url : ACC.radicaDelinDetallesURL,
@@ -37,21 +37,98 @@ ACC.duBotonVer = {
 	
 	fillFieldsFromData : function(data) {
 
-		var tipoValla = $("#selectedTipoValla").val();
-
-		$("#divInfoRadicados").show();	
+		$("#divInfoRadicados").show();
+		debugger;
 		$("#duRadicados").find("tr:gt(0)").remove();
 		$.each(data.radicados, function (index,value){
+			if(value.numRadicado != ''){
 			$('#duRadicados').append("<tr>"+ 
 					 '<td><input id="numRadicado" name="" class="inputtextnew" aria-required="true" type="text" readonly="readonly" maxlength="240" style="width: 100% !important; padding-left: 1px !important; padding-right: 1px !important;" value="' + value.numRadicado + '"></td>"' +
 					 '<td><input id="fechaRadicado" name="" class="inputtextnew" aria-required="true" type="text" readonly="readonly" maxlength="240" style="width: 100% !important; padding-left: 1px !important; padding-right: 1px !important;" value="' + value.fechaRadicado + '"></td>"' +
 					 '<td><input id="modLicencia" name="" class="inputtextnew" aria-required="true" type="text" readonly="readonly" maxlength="240" style="width: 100% !important; padding-left: 1px !important; padding-right: 1px !important;" value="' + value.modLicencia + '"></td>"' +
 					 '<td><input id="presupuesto" name="" class="inputtextnew" aria-required="true" type="text" readonly="readonly" maxlength="240" style="width: 100% !important; padding-left: 1px !important; padding-right: 1px !important;" value="' + value.presupuesto + '"></td>"' +
 					 '<td><input id="estrato" name="" class="inputtextnew" aria-required="true" type="text" readonly="readonly" maxlength="240" style="width: 100% !important; padding-left: 1px !important; padding-right: 1px !important;" value="' + value.estrato + '"></td>"' +
-					 '<td><input class="inputtextnew" style="visibility: visible !important; width: 20% !important" type="checkbox" id="selec"></td> '+ 
-					 "</tr>");	
+					 '<td><input class="inputtextnew selectRadicado" style="visibility: visible !important; width: 20% !important" type="checkbox"  data-numradicado="' + value.numRadicado + '" data-modlicencia="' + value.modLicencia +'" ></td> '+ 
+					 "</tr>");	}
 		})
-	}
+	},
+	
+	
+	bindCheckBoxSelect : function() {
+		$(document).on("click", ".selectRadicado", function(e) {
+//			e.preventDefault();
+			
+ 	       var valorNumRadicado = $.trim($(this).attr("data-numradicado")); 	       
+ 	       var valorModLicencia = $.trim($(this).attr("data-modlicencia")); 	       
+
+ 	       $("#selectedRadicado").val(valorNumRadicado);
+ 	       $("#selectedTipoLicencia").val(valorModLicencia);
+
+		});
+	},
+	
+	
+	bindGeneraDeclaracionButton: function () {
+		 $(document).on("click", "#duGeneraDeclaracionButton", function (e) {
+	 	        e.preventDefault();
+	 	        
+	 	       var numForm  = $.trim($("#numForm").val());
+	 	 
+	 	       var data = {};
+	 	       
+	 	       data.numForm=numForm;
+	 	
+	 	      $.ajax({
+		            url: ACC.duGeneraDeclaracionURL,
+		            data: data,
+		            type: "GET",
+		            success: function (data) {
+		            	$( "#dialogDU" ).dialog( "open" );
+		            	if(data.errores)
+	            		{
+		            		$("#duDialogContent").html("");
+		            		$.each(data.errores, function( index, value ) {
+   	            			$("#duDialogContent").html($("#duDialogContent").html()+value.txtmsj+"<br>");
+   	            		});
+		            		
+		            		
+	            		}else
+	            		{
+	            			$("#duDialogContent").html("");
+	            			$("#duDialogContent").html("La declaración se ha generado exitosamente.")
+	            			
+	            			$("#downloadHelper").attr("href",data.urlDownload);
+	            			
+	            			document.getElementById("downloadHelper").click();
+	            			
+	            		}
+	 	      		
+		            },
+		            error: function () {
+		            	$( "#dialogDU" ).dialog( "open" );
+		            	$("#duDialogContent").html("Hubo un error al generar la declaración, por favor inténtalo más tarde");
+		            }
+		        });
+	 	       
+		 });
+	 },
+	 
+	
+   
+	    bindDialogGasolina: function(){
+   	
+   	$( "#dialogDU" ).dialog({ 
+   		autoOpen: false, 
+   		modal: true,
+			 draggable: false,
+   		buttons: {
+   			Ok: function() {
+   				$( this ).dialog( "close" );
+   			}
+   	    } 
+   	});
+   	
+   }
 
 
 };
