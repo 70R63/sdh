e2ba705fb6a101deb95c6fb3976643e898ca8ad6
jair roa <jair.r04@gmail.com>
2019-07-31 22:24:57 -0500

    Preguntas Reto

--- hybris/hybris/bin/custom/sdh/sdhstorefront/web/webroot/_ui/responsive/common/js/acc.tramitesSeleccion.js
+++ hybris/hybris/bin/custom/sdh/sdhstorefront/web/webroot/_ui/responsive/common/js/acc.tramitesSeleccion.js
@@ -1,6 +1,6 @@
 ACC.tramitesSeleccion = {
 
-	_autoload : [ "bindTramitesSelect" ],
+	_autoload : [ "bindTramitesSelect", "bindTramitesEnviar" ],
 
 	
 	bindTramitesSelect : function() {
@@ -16,42 +16,25 @@ ACC.tramitesSeleccion = {
 			dataActual.valorN2 = $("#selectNivel2").val();
 			dataActual.valorN3 = $("#selectNivel3").val();
 
-			debugger;
+//			debugger;
 			var doc = document.getElementById('documentos');
 			doc.style.display='none';
 			
 			if(dataActual.nivelSeleccion == 0){
-				$("#divSubCategoria").hide();
-				$("#divCategoria").hide();
-				$("#divSubtramite").hide();
-				$("#selectNivel1").find("option:gt(0)").remove();
-				$("#selectNivel1").find("option:eq(0)").remove();
-				$("#selectNivel2").find("option:gt(0)").remove();
-				$("#selectNivel2").find("option:eq(0)").remove();
-				$("#selectNivel3").find("option:gt(0)").remove();
-				$("#selectNivel3").find("option:eq(0)").remove();
+				ACC.tramitesSeleccion.clearFieldsFromDataSelN0();
 			}
 
 			if(dataActual.nivelSeleccion == 1){
-				$("#divSubCategoria").hide();
-				$("#divCategoria").hide();
-				$("#selectNivel2").find("option:gt(0)").remove();
-				$("#selectNivel2").find("option:eq(0)").remove();
-				$("#selectNivel3").find("option:gt(0)").remove();
-				$("#selectNivel3").find("option:eq(0)").remove();			
+				ACC.tramitesSeleccion.clearFieldsFromDataSelN1();		
 			}
 
 			if(dataActual.nivelSeleccion == 2){
-				$("#divSubCategoria").hide();
-				$("#selectNivel3").find("option:gt(0)").remove();
-				$("#selectNivel3").find("option:eq(0)").remove();
+				ACC.tramitesSeleccion.clearFieldsFromDataSelN2();
 			}
-				
 
 			
-			
 			$.ajax({
-				url : ACC.tramitestSeleccionURL,
+				url : ACC.casoSeleccionURL,
 				data : dataActual,
 				type : "GET",
 				success : function(dataResponse) {
@@ -63,20 +46,97 @@ ACC.tramitesSeleccion = {
 		});
 	},
 	
+	bindTramitesEnviar : function() {
+		$(document).on("click", ".tramitesEnviar", function(e) {
+			e.preventDefault();
+						
+			
+ 	        var valorNivel = 0; 	       
+			var valorActual = this.value;
+			var validacion = false;
+			var dataActual = {};
+			
+			dataActual.nivelSeleccion = valorNivel;
+			dataActual.valorN0 = $("#selectNivel0").val();
+			dataActual.valorN1 = $("#selectNivel1").val();
+			dataActual.valorN2 = $("#selectNivel2").val();
+			dataActual.valorN3 = $("#selectNivel3").val();
+			dataActual.mensaje = $("#mensaje").val();			
+			
+			debugger;
+			validacion = ACC.tramitesSeleccion.validarInfoAntesSubmit(dataActual);
+			
+			if(validacion == true){
+				$.ajax({
+					url : ACC.casoCreacionURL,
+					data : dataActual,
+					type : "GET",
+					success : function(dataResponse) {
+						ACC.tramitesSeleccion.resultadoCreacionCaso(dataActual,dataResponse);
+					},
+					error : function() {
+					}
+				});
+			}
+			
+		});
+	},
+	
+	validarInfoAntesSubmit : function(infoActual,infoResponse) {
+
+		debugger;
+		var mensaje = "";
+		var validacion = true;
+		
+		
+		if(infoActual.valorN0 != null){
+			if(infoActual.valorN0 == "00"){
+				validacion = false;
+			}else{
+				if(infoActual.valorN1 != null){
+					if(infoActual.valorN1 == "00"){
+						validacion = false;
+					}else{
+						if(infoActual.valorN2 != null){
+							if(infoActual.valorN2 == "00"){
+								validacion = false;
+							}else{
+								if(infoActual.valorN3 != null){
+									if(infoActual.valorN3 == "00"){
+										validacion = false;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}else{
+			validacion = false;
+		}
+		
+		if(infoActual.mensaje != null){
+			if(infoActual.mensaje == ""){
+				validacion = false;
+			}
+		}else{
+			validacion = false;
+		}
+		
+		if(validacion == false){
+			alert("Por favor ingresar los valores obligatorios marcados con *");
+		}
+		
+		return validacion;		
+	},
+	
+	
 	fillFieldsFromDataSel : function(infoActual,infoResponse) {
 
-		
+		var doc = document.getElementById('documentos');
+		doc.style.display='none';
 		if(infoActual.nivelSeleccion == 0){
-			$("#divSubCategoria").hide();
-			$("#divCategoria").hide();
-			$("#divSubtramite").hide();
-			$("#selectNivel1").find("option:gt(0)").remove();
-			$("#selectNivel1").find("option:eq(0)").remove();
-			$("#selectNivel2").find("option:gt(0)").remove();
-			$("#selectNivel2").find("option:eq(0)").remove();
-			$("#selectNivel3").find("option:gt(0)").remove();
-			$("#selectNivel3").find("option:eq(0)").remove();
-
+			ACC.tramitesSeleccion.clearFieldsFromDataSelN0();
 			
 			if(infoResponse.opciones.length > 0){
 				$.each(infoResponse.opciones, function (index,value){
@@ -87,13 +147,8 @@ ACC.tramitesSeleccion = {
 		}
 
 		if(infoActual.nivelSeleccion == 1){
-			$("#divSubCategoria").hide();
-			$("#divCategoria").hide();
-			$("#selectNivel2").find("option:gt(0)").remove();
-			$("#selectNivel2").find("option:eq(0)").remove();
-			$("#selectNivel3").find("option:gt(0)").remove();
-			$("#selectNivel3").find("option:eq(0)").remove();			
-
+			ACC.tramitesSeleccion.clearFieldsFromDataSelN1();
+			
 			if(infoResponse.opciones.length > 0){
 				$.each(infoResponse.opciones, function (index,value){
 					$('#selectNivel2').append("<option value="+ value.key+">"+value.label+"</option>");
@@ -103,9 +158,7 @@ ACC.tramitesSeleccion = {
 		}
 
 		if(infoActual.nivelSeleccion == 2){
-			$("#divSubCategoria").hide();
-			$("#selectNivel3").find("option:gt(0)").remove();
-			$("#selectNivel3").find("option:eq(0)").remove();
+			ACC.tramitesSeleccion.clearFieldsFromDataSelN2();
 			
 			if(infoResponse.opciones.length > 0){
 				$.each(infoResponse.opciones, function (index,value){
@@ -116,23 +169,8 @@ ACC.tramitesSeleccion = {
 		}
 		
 		
-		debugger;
-		var doc = document.getElementById('documentos');
-		var docDescripcion = document.getElementById('docTramite');
-		if(infoResponse.docTramitesResponse != null){
-			if(infoResponse.docTramitesResponse.documentos != null){
-				if(infoResponse.docTramitesResponse.documentos.tipoDocumen_t != null){
-					docDescripcion.value = infoResponse.docTramitesResponse.documentos.tipoDocumen_t;		
-					doc.style.display='block';
-				}else{
-					doc.style.display='none';
-				}						
-			}else{
-				doc.style.display='none';
-			}
-		}else{
-			doc.style.display='none';
-		}
+		ACC.tramitesSeleccion.fillFieldsFromDataDocs(infoResponse);
+
 		
 				
 //		debugger;
@@ -151,56 +189,77 @@ ACC.tramitesSeleccion = {
 		
 	},
 	
-	
-	clearFieldsFromDataSel : function(infoActual) {
+	resultadoCreacionCaso : function(infoActual,infoResponse) {
 
-		var doc = document.getElementById('documentos');
-		doc.style.display='none';	
-		if(infoActual.nivelSeleccion == 0){
-			$("#divSubCategoria").hide();
-			$("#divCategoria").hide();
-			$("#divSubtramite").hide();
-			$("#selectNivel1").find("option:gt(0)").remove();
-			$("#selectNivel1").find("option:eq(0)").remove();
-			$("#selectNivel2").find("option:gt(0)").remove();
-			$("#selectNivel2").find("option:eq(0)").remove();
-			$("#selectNivel3").find("option:gt(0)").remove();
-			$("#selectNivel3").find("option:eq(0)").remove();
-		}
-
-		if(infoActual.nivelSeleccion == 1){
-			$("#divSubCategoria").hide();
-			$("#divCategoria").hide();
-			$("#selectNivel2").find("option:gt(0)").remove();
-			$("#selectNivel2").find("option:eq(0)").remove();
-			$("#selectNivel3").find("option:gt(0)").remove();
-			$("#selectNivel3").find("option:eq(0)").remove();			
-		}
-
-		if(infoActual.nivelSeleccion == 2){
-			$("#divSubCategoria").hide();
-			$("#selectNivel3").find("option:gt(0)").remove();
-			$("#selectNivel3").find("option:eq(0)").remove();
-		}
+		debugger;
+		var mensaje = "";
 		
-
+		if(infoResponse.respuesta.num_caso != null){
+			mensaje = infoResponse.respuesta.descripcion + infoResponse.respuesta.num_caso;
+		}else{
+			mensaje = infoResponse.respuesta.descripcion;
+		}
+		alert(mensaje);
+		
+		
 	},
 	
 	
 	fillFieldsFromDataDocs : function(docTramites) {
 
 		debugger;
-		var descripcionDocumento = docTramites.documentos.tipoDocumen_t;
 		var doc = document.getElementById('documentos');
+		var mostrarTabDocs = false;
 		
-		if(descripcionDocumento != null){
-			$("#docTramite").val = descripcionDocumento;		
+		
+		$("#documentos").find("tr:gt(0)").remove();
+		$.each(docTramites.docTramitesResponse.documentos, function (index,value){
+			if(value.tipoDocumen_t != ''){
+			$('#documentos').append("<tr>"+ 
+					 '<td><input class="inputtextnew" disabled="disabled" type="text" size="30" id = "docTramite" value="'+ value.tipoDocumen_t + '"/></td>"'+
+					 '<td><input class="inputtextnew" type="file" size="30" /></td>'+
+					 "</tr>");
+			mostrarTabDocs = true;
+			}
+		});
+		if(mostrarTabDocs == true){
 			doc.style.display='block';
 		}else{
 			doc.style.display='none';
 		}
-
+	
+	},
+	
+	clearFieldsFromDataSelN0 : function(infoActual) {
 		
+		$("#divSubCategoria").hide();
+		$("#divCategoria").hide();
+		$("#divSubtramite").hide();
+		$("#selectNivel1").find("option:gt(0)").remove();
+		$("#selectNivel1").find("option:eq(0)").remove();
+		$("#selectNivel2").find("option:gt(0)").remove();
+		$("#selectNivel2").find("option:eq(0)").remove();
+		$("#selectNivel3").find("option:gt(0)").remove();
+		$("#selectNivel3").find("option:eq(0)").remove();
+
+	},
+	
+	clearFieldsFromDataSelN1 : function(infoActual) {
+
+		$("#divSubCategoria").hide();
+		$("#divCategoria").hide();
+		$("#selectNivel2").find("option:gt(0)").remove();
+		$("#selectNivel2").find("option:eq(0)").remove();
+		$("#selectNivel3").find("option:gt(0)").remove();
+		$("#selectNivel3").find("option:eq(0)").remove();			
+	
+	},
+	
+	clearFieldsFromDataSelN2 : function(infoActual) {
+		$("#divSubCategoria").hide();
+		$("#selectNivel3").find("option:gt(0)").remove();
+		$("#selectNivel3").find("option:eq(0)").remove();
+	
 	}
 	
 
