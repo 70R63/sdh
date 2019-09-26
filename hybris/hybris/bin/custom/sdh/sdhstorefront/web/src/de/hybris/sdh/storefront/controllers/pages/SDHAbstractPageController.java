fb0a64ee87c6f343c8ec46daeb9caf9a47e7b28f
jcarrillo <jesus.carrillo1987@gmail.com>
2019-09-26 13:12:22 -0500

    fix para no mostrar el boton de firmar y agregar cuando hay 3 renglones de firmantes

--- hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/pages/SDHAbstractPageController.java
+++ hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/pages/SDHAbstractPageController.java
@@ -74,8 +74,12 @@ public class SDHAbstractPageController extends AbstractSearchPageController {
         for(FirmanteResponse eachFirmante : firmantes){
             if(eachFirmante.getNumIdent().equalsIgnoreCase(representanteData.getDocumentNumber())){
                 showNewFirmRow=false;
-                
+
             }
+        }
+
+        if(CollectionUtils.isNotEmpty(firmantes) && firmantes.size() == 2 && showNewFirmRow == true){
+            showFirmAndAddButton = false;
         }
 
         model.addAttribute("showFirmButton",showFirmButton);
