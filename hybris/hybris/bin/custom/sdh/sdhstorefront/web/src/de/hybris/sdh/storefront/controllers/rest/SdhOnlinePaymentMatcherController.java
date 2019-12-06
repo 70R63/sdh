ee0856819098bf47e0c954a15e6a53ba37827bba
eduardocrns <eduardocrns@gmail.com>
2019-12-06 17:45:40 -0500

    Ajustes para lista de bancos

--- hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/rest/SdhOnlinePaymentMatcherController.java
+++ hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/controllers/rest/SdhOnlinePaymentMatcherController.java
@@ -1,16 +1,20 @@
 package de.hybris.sdh.storefront.controllers.rest;
 
-import de.hybris.sdh.core.enums.SdhPaymentMethodTypeEnum;
-import de.hybris.sdh.core.enums.SdhTaxTypesEnum;
+import de.hybris.platform.servicelayer.config.ConfigurationService;
+import de.hybris.sdh.core.soap.pse.PseServices;
+import de.hybris.sdh.core.soap.pse.beans.ConstantConnectionData;
+import de.hybris.sdh.core.soap.pse.eanucc.GetBankListResponseInformationType;
+import de.hybris.sdh.core.soap.pse.impl.MessageHeader;
 import de.hybris.sdh.facades.online.payment.data.OnlinePaymentSelectInputBoxData;
 import de.hybris.sdh.facades.online.payment.impl.DefaultSDHOnlinePaymentProviderMatcherFacade;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import java.util.List;
-
 @RestController
 @RequestMapping("/onlinePaymentMatcher")
 public class SdhOnlinePaymentMatcherController {
@@ -18,12 +22,41 @@ public class SdhOnlinePaymentMatcherController {
     @Resource(name = "sdhOnlinePaymentProviderMatcherFacade")
     DefaultSDHOnlinePaymentProviderMatcherFacade sdhOnlinePaymentProviderMatcherFacade;
 
+	@Resource(name = "configurationService")
+	private ConfigurationService configurationService;
+
+	@Resource(name = "defaultPseServices")
+	private PseServices pseServices;
+
 
     @RequestMapping("/getBanks")
-    public List<OnlinePaymentSelectInputBoxData> getBanks(@RequestParam(value="tax", defaultValue="") String tax,
-                                                          @RequestParam(value="paymentMethod", defaultValue="") String paymentMethod) {
+    public List<OnlinePaymentSelectInputBoxData> getBanks(@RequestParam(value="tax", defaultValue="") final String tax,
+                                                          @RequestParam(value="paymentMethod", defaultValue="") final String paymentMethod) {
 
-       return sdhOnlinePaymentProviderMatcherFacade.getBankList(tax,paymentMethod);
+
+		final GetBankListResponseInformationType[] bankList = pseServices.getBankList(this.getConstantConnectionData(),
+				this.getMessageHeader());
+
+		return sdhOnlinePaymentProviderMatcherFacade.getBankList(tax, paymentMethod);
 
     }
+
+	private MessageHeader getMessageHeader()
+	{
+		final MessageHeader messageHeader = new MessageHeader();
+		messageHeader.setTo(configurationService.getConfiguration().getString("sdh.pse.messageHeader.to"));
+		messageHeader.setFrom(configurationService.getConfiguration().getString("sdh.pse.messageHeader.from"));
+		messageHeader
+				.setRepresentingParty(configurationService.getConfiguration().getString("sdh.pse.messageHeader.representingParty"));
+		return messageHeader;
+	}
+
+	private ConstantConnectionData getConstantConnectionData()
+	{
+		final ConstantConnectionData constantConnectionData = new ConstantConnectionData();
+		constantConnectionData.setPseurl(configurationService.getConfiguration().getString("sdh.pse.pseURL"));
+		constantConnectionData.setPpeCode(configurationService.getConfiguration().getString("sdh.pse.ppeCode"));
+		return constantConnectionData;
+	}
+
 }
