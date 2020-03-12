e85064870b0d981f2b29ae3f34b3bbb82df7093b
federicofd <federicofd@yahoo.com>
2020-03-12 15:23:23 -0600

    Predial (declaracion opciones 1-8) mostrar/ocultar botones de presentar declaracion, pagar en linea dependiendo del tipo de documento del contribuyente

---
+++ hybris/hybris/bin/custom/sdh/sdhstorefront/web/src/de/hybris/sdh/storefront/forms/PredialControlCamposDec.java
@@ -0,0 +1,52 @@
+/**
+ *
+ */
+package de.hybris.sdh.storefront.forms;
+
+/**
+ * @author Federico Flores Dimas
+ *
+ */
+public class PredialControlCamposDec
+{
+
+	private boolean btnPresentarDec;
+	private boolean btnPagarDec;
+
+
+	/**
+	 * @return the btnPresentarDec
+	 */
+	public boolean isBtnPresentarDec()
+	{
+		return btnPresentarDec;
+	}
+
+	/**
+	 * @param btnPresentarDec
+	 *           the btnPresentarDec to set
+	 */
+	public void setBtnPresentarDec(final boolean btnPresentarDec)
+	{
+		this.btnPresentarDec = btnPresentarDec;
+	}
+
+	/**
+	 * @return the btnPagarDec
+	 */
+	public boolean isBtnPagarDec()
+	{
+		return btnPagarDec;
+	}
+
+	/**
+	 * @param btnPagarDec
+	 *           the btnPagarDec to set
+	 */
+	public void setBtnPagarDec(final boolean btnPagarDec)
+	{
+		this.btnPagarDec = btnPagarDec;
+	}
+
+
+}
