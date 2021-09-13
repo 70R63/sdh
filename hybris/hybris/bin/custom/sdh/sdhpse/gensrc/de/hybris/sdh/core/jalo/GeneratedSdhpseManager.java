ecc04717f3629ae62d7247039e511d01e217dc42
emartinezperez <85138834+emartinezperez@users.noreply.github.com>
2021-09-13 15:51:44 -0500

    Ajuste para desplegar infoobjeto de Delineacion para Agente Autorizado

--- hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/jalo/GeneratedSdhpseManager.java
+++ hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/jalo/GeneratedSdhpseManager.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 9 sep 2021 20:49:00                         ---
+ * --- Generated at 13 sep 2021 15:35:22                        ---
  * ----------------------------------------------------------------
  *  
  * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
