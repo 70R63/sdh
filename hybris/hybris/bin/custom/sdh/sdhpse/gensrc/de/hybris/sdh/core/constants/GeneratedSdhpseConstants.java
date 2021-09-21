ff59317ef8e2d44186efe26dc19787f6d10c5e9a
emartinezperez <85138834+emartinezperez@users.noreply.github.com>
2021-09-20 20:46:08 -0500

    Ajuste para desplegar la pagina de estado de cuenta con un response vacio

--- hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/constants/GeneratedSdhpseConstants.java
+++ hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/constants/GeneratedSdhpseConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 20 sep 2021 14:20:38                        ---
+ * --- Generated at 20 sep 2021 20:20:28                        ---
  * ----------------------------------------------------------------
  *  
  * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
