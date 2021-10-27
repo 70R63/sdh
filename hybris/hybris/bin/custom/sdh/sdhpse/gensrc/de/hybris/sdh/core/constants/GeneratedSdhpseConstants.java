3de6d0d2409701d95a961f66ac16fe771f2a4f7c
emartinezperez <85138834+emartinezperez@users.noreply.github.com>
2021-10-27 12:54:40 -0500

    Ajuste para texto de terminos y condiciones

--- hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/constants/GeneratedSdhpseConstants.java
+++ hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/constants/GeneratedSdhpseConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 26 oct 2021 14:13:17                        ---
+ * --- Generated at 27 oct 2021 11:39:59                        ---
  * ----------------------------------------------------------------
  *  
  * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
