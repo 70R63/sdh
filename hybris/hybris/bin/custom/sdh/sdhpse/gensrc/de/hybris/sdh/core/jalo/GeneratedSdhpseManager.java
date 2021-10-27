fc6804ca4429dfc0799d0b2de7be0ca36060e82a
emartinezperez <85138834+emartinezperez@users.noreply.github.com>
2021-10-27 14:31:06 -0500

    Ajuste para titulo Gasolina en estado de cuenta

--- hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/jalo/GeneratedSdhpseManager.java
+++ hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/jalo/GeneratedSdhpseManager.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 27 oct 2021 12:57:15                        ---
+ * --- Generated at 27 oct 2021 14:08:14                        ---
  * ----------------------------------------------------------------
  *  
  * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
