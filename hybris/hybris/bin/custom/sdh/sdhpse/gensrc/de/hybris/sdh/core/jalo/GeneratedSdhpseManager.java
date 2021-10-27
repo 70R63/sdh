31c16a9ae8bd83c944886d017ea3fc4caa399d4f
emartinezperez <85138834+emartinezperez@users.noreply.github.com>
2021-10-27 14:06:17 -0500

    Quitar ====== de obligaciones pendientes Gasolina

--- hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/jalo/GeneratedSdhpseManager.java
+++ hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/jalo/GeneratedSdhpseManager.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 26 oct 2021 21:10:17                        ---
+ * --- Generated at 27 oct 2021 13:45:19                        ---
  * ----------------------------------------------------------------
  *  
  * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
