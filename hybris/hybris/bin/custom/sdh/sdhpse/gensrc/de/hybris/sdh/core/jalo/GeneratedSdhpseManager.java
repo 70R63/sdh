93152546824418eedf2f48457710fcae06d5d6d1
emartinezperez <85138834+emartinezperez@users.noreply.github.com>
2021-11-16 13:43:32 -0600

    Ajuste para quitar Valida cont de infoobjeto de publicidad y etiquetas de de estado de cuenta

--- hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/jalo/GeneratedSdhpseManager.java
+++ hybris/hybris/bin/custom/sdh/sdhpse/gensrc/de/hybris/sdh/core/jalo/GeneratedSdhpseManager.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 9 nov 2021 20:22:09                         ---
+ * --- Generated at 16 nov 2021 12:39:30                        ---
  * ----------------------------------------------------------------
  *  
  * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
